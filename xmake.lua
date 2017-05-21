f927b04cbc5502a0590592424c9e71a800a40d12
745275633 <DAStudioDirector@gmail.com>
2017-05-21 21:57:02 +0800

    DA-exception sudo

--- xmake.lua
+++ xmake.lua
@@ -41,7 +41,7 @@ target("factorio-mod-info")
 			os.exit(1);
 		end
 		if not os.exists("/usr/include/DA/exception.hpp") then
-			os.run("sudo $(scriptdir)/DA-exception/install")
+			os.run("cd $(scriptdir)/DA-exception && sudo ./install")
 		end
 	end)
 
