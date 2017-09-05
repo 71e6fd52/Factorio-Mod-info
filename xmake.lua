889b5de9ce65abb8e0dc353717cbd7e1ed342441
745275633 <DAStudioDirector@gmail.com>
2017-09-05 21:03:22 +0800

    xmake pacaur

--- xmake.lua
+++ xmake.lua
@@ -8,11 +8,14 @@ add_links("ssl", "pthread", "boost_date_time", "boost_system", "boost_filesystem
 add_includedirs("$(projectdir)/include")
 
 before_build(function (target)
-  assert(os.isdir("/usr/include/boost"), "Need boost\nInstall it with package manager\nOr visit http://www.boost.org/")
-  assert(os.isdir("/usr/include/avhttp") and os.exists("/usr/include/avhttp.hpp"), "Need avhttp\nOr visit https://avplayer.org/avhttp.html")
-  if not os.exists("/usr/include/DA/exception.hpp") then
-    os.exec("sh -c \'cd $(scriptdir)/DA-exception && sudo ./install\'")
+  --assert(os.host() == "linux", "Maybe you can help me to support other system")
+  if (os.exists("/usr/bin/pacaur")) then
+    cprint("${bright}Why not use `pacaur -S factorio-mod-info`")
+    cprint("${bright}If you still want to build, you can run `pacaur -S boost avhttp-git da-exception-git` to install depends")
   end
+  assert(os.isdir("/usr/include/boost"), "Need boost\nInstall it with package manager\nOr visit http://www.boost.org/")
+  assert(os.isdir("/usr/include/avhttp") and os.exists("/usr/include/avhttp.hpp"), "Need avhttp\nVisit https://avplayer.org/avhttp.html")
+  assert(os.exists("/usr/include/DA/exception.hpp"), "Need DA-exception\nVisit https://github.com/DATechnologyStudio/DA-exception")
 end)
 
 target("factorio-mod-info")
